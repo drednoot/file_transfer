4ba8104c98ba41f2773ed0bbcd6a5b2d5392cc42
ns_gbljh <matveyerohin228@gmail.com>
2024-07-10 11:05:18 +0400

    added message boxes

--- src/server/server.cc
+++ src/server/server.cc
@@ -25,8 +25,9 @@ Server::Server(QWidget *parent, const int port, const QString &files_path)
 bool Server::InitServer() {
   qtcp_serv_ = new QTcpServer(this);
   if (!qtcp_serv_->listen(QHostAddress::LocalHost, port_)) {
-    // TODO QMessageBox
-    close();
+    QMessageBox::critical(
+        this, tr("File Transfer Server"),
+        tr("Unable to start the server: %1.").arg(qtcp_serv_->errorString()));
     return false;
   }
 
@@ -35,9 +36,17 @@ bool Server::InitServer() {
 
 bool Server::InitFilesFolder(const QString &files_path) {
   QDir dir = QDir::current();
-  if (!dir.mkdir(files_path) || !dir.cd(files_path)) {
-    // TODO QMessageBox
-    close();
+  if (!dir.mkdir(files_path) && !dir.exists(files_path)) {
+    QMessageBox::critical(
+        this, tr("File Transfer Server"),
+        tr("Unable to start the server: %1.").arg("Couldn't make files dir"));
+    return false;
+  }
+
+  if (!dir.cd(files_path)) {
+    QMessageBox::critical(this, tr("File Transfer Server"),
+                          tr("Unable to start the server: %1.")
+                              .arg("Couldn't move to files dir"));
     return false;
   }
 
